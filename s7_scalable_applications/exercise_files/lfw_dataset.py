7d5c2dd7c5a1adbfdaec912dc6cb03ab36deef0b
Laurine <dargaud.laurine@gmail.com>
2022-01-18 17:03:27 +0100

    complete s7 M19 exercice with distributed data loading

--- s7_scalable_applications/exercise_files/lfw_dataset.py
+++ s7_scalable_applications/exercise_files/lfw_dataset.py
@@ -10,24 +10,48 @@ from PIL import Image
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 
+import os
+from torchvision.utils import make_grid, save_image
+from matplotlib.pyplot import errorbar
+
 
 class LFWDataset(Dataset):
     def __init__(self, path_to_folder: str, transform) -> None:
         # TODO: fill out with what you need
         self.transform = transform
+        self.root_dir = path_to_folder
+
+        self.image_paths = []
+        self.labels = []
+
+        for name in os.listdir(path_to_folder):
+            name_folder = os.path.join(path_to_folder, name)
+            for image in os.listdir(name_folder):
+                image_file = os.path.join(name_folder, image)
+                self.image_paths.append(image_file)
+                self.labels.append(name_folder)
+
         
     def __len__(self):
-        return None # TODO: fill out
+        return len(self.image_paths)
     
     def __getitem__(self, index: int) -> torch.Tensor:
         # TODO: fill out
-        return self.transform(img)
+        image_file = self.image_paths[index]
+        with Image.open(image_file, 'r') as img:
+            return self.transform(img), self.labels[index]
 
         
 if __name__ == '__main__':
+
+    default_path_to_folder = '/mnt/c/Users/Laurine/Documents/DTU Python/Machine Learning Operations/lfw-deepfunneled'
+    batch_size = 512
+    #default_path_to_folder = '/mnt/c/Users/Laurine/Documents/DTU Python/Machine Learning Operations/lfw-deepfunneled-light'
+    #batch_size = 5
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('-path_to_folder', default='', type=str)
-    parser.add_argument('-num_workers', default=None, type=int)
+    parser.add_argument('-path_to_folder', default=default_path_to_folder, type=str)
+    parser.add_argument('-num_workers', default=2, type=int)
     parser.add_argument('-visualize_batch', action='store_true')
     parser.add_argument('-get_timing', action='store_true')
     args = parser.parse_args()
@@ -43,12 +67,15 @@ if __name__ == '__main__':
     # Define dataloader
     # Note we need a high batch size to see an effect of using many
     # number of workers
-    dataloader = DataLoader(dataset, batch_size=512, shuffle=False,
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False,
                             num_workers=args.num_workers)
     
     if args.visualize_batch:
         # TODO: visualize a batch of images
-        pass
+        images, labels = next(iter(dataloader))
+        grid = make_grid(images)
+        save_image(grid, 'batch_visualization.jpg')
+
         
     if args.get_timing:
         # lets do so repetitions
@@ -63,4 +90,10 @@ if __name__ == '__main__':
             res.append(end - start)
             
         res = np.array(res)
-        print('Timing: {np.mean(res)}+-{np.std(res)}')
+        mean, std = np.mean(res), np.std(res)
+        print(f'Timing: {mean}+-{std}')
+        
+        # save timing result
+        with open("timing.csv", "a") as f:
+            f.write(f"{args.num_workers};{mean};{std}\n")
+
